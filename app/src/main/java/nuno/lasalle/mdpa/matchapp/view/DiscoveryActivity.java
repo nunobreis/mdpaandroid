f101f95ec8b55346d88fb83621fa926c8679ab12
nunobreis <nuno@catalpa.io>
2018-02-05 15:11:43 +0100

    redirects the loggedin user to the discovery view

--- app/src/main/java/nuno/lasalle/mdpa/matchapp/view/DiscoveryActivity.java
+++ app/src/main/java/nuno/lasalle/mdpa/matchapp/view/DiscoveryActivity.java
@@ -1,10 +1,21 @@
 package nuno.lasalle.mdpa.matchapp.view;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import nuno.lasalle.mdpa.matchapp.R;
 
 /**
  * Created by nunoreis on 05/02/2018.
  */
 
 public class DiscoveryActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discovery);
+    }
 }
+
