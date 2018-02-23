53a286ec56500eb76e41b1a44276b27a70fe06ce
nunobreis <nuno@catalpa.io>
2018-02-23 15:53:16 +0100

    implements a settings view

--- app/src/main/java/nuno/lasalle/mdpa/matchapp/view/SettingsFragment.java
+++ app/src/main/java/nuno/lasalle/mdpa/matchapp/view/SettingsFragment.java
@@ -3,11 +3,15 @@ package nuno.lasalle.mdpa.matchapp.view;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.FragmentManager;
 import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 import nuno.lasalle.mdpa.matchapp.R;
 
@@ -35,4 +39,5 @@ public class SettingsFragment extends Fragment {
         return view;
 
     }
+
 }
